e296e1b686bcb64b3901cc8e5225e520784ba397
Biju Chacko <biju.ch@directi.com>
2013-12-18 13:24:58 +0530

    Inertr now bulk pushes data

--- insertr.py
+++ insertr.py
@@ -4,31 +4,37 @@ import cql
 import random
 from datetime import datetime
 import pytz
-import tornado.options 
+from tornado.options import define, options, parse_command_line
+import time
+import sys
+
+define("count", default=10, help="Number of metrics to insert")
+parse_command_line()
 
 cluster = '127.0.0.1'
+port = 9160
 keyspace = 'meetr'
 
-con = cql.connect(cluster, 9160,  keyspace, cql_version='3.0.0')
-print ("Connected!")
+con = cql.connect(cluster, port,  keyspace, cql_version='3.0.0')
 cursor = con.cursor()
 
-instance_id = "hostname"
-metric = "bytes_out"
-value = random.randint(128,512)
-
-utc = pytz.utc
-time_stamp = utc.localize(datetime.utcnow()).strftime('%Y-%m-%d %H:%M:%S %z')
 
 cql_template = """INSERT INTO metrics (
-    instance_id, 
     metric, 
     collected_at,
     metric_value
-    ) VALUES ('{0}', '{1}', '{2}', {3});"""
-cql = cql_template.format(instance_id,metric,time_stamp,value)
+    ) VALUES (:metric_, :collected_at_, :metric_value_);"""
 
-print(cql)
+instance_id = "hostname"
+metric = "hostname.bytes_out"
 
+utc = pytz.utc
+for c in range(options.count):
+    value = random.randint(128,512)
+    time_stamp = utc.localize(datetime.utcnow()).strftime('%Y-%m-%d %H:%M:%S %z')
+    cursor.execute(cql_template, dict(metric_=metric, collected_at_=time_stamp, metric_value_=value))
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    time.sleep(1)
 
-cursor.execute(cql)
+print ""
